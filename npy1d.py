525c88273d8bf196e57eadfe3b70fe32aed73412
Johan <lukmanjohan854@gmail.com>
2024-03-23 13:53:47 +0700

    belajar

---
+++ npy1d.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a = np.arange(21)
+
+result = a[3:16:3]
+
+print("Array 1 dimensi:")
+print(a)
+print("Nilai dari index ke-3 sampai ke-15 dengan step kelipatan 3:")
+print(result)
---
+++ npy2d.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([[12,32],
+	         [86,40]])
+
+print(a)
+print("jumlah dimensi {}".format(a.ndim))
+print(a[0][1])
